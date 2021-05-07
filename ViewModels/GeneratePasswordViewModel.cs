e3bd3cf864dd4a920b4a8e7f7e5d59094edda8fb
Catishere <viktor.gyoshev@gmail.com>
2021-05-07 09:02:40 +0300

    Added copy to clipboard button on random password generator

--- ViewModels/GeneratePasswordViewModel.cs
+++ ViewModels/GeneratePasswordViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 using PasswordHoarder.Models.UI;
 using PasswordHoarder.Stores;
@@ -13,11 +14,17 @@ namespace PasswordHoarder.ViewModels
     class GeneratePasswordViewModel : IViewModel
     {
         public ICommand NavigateBackCommand { get; }
+        public ICommand CopyCommand { get; set; }
 
         public PasswordGenerator PasswordGenerator { get; set; }
 
         public GeneratePasswordViewModel(NavigationStore navigationStore)
         {
+            
+            CopyCommand = new GenericCommand<object>
+            {
+                ExecuteDelegate = _ => Clipboard.SetText(PasswordGenerator.Password)
+            };
             PasswordGenerator = new PasswordGenerator();
             NavigateBackCommand = new NavigationCommand<BrowserViewModel>(navigationStore);
         }
